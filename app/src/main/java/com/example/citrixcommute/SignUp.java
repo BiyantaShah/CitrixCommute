463e935cad88388c7fc1ee2e77eebc8c8f652cc6
Biyanta Shah <shah.bi@husky.neu.edu>
2019-08-11 11:31:24 -0400

    Added database for signup just name field to test

--- app/src/main/java/com/example/citrixcommute/SignUp.java
+++ app/src/main/java/com/example/citrixcommute/SignUp.java
@@ -6,15 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+
+    private static final String TAG = "SignUp";
+    DatabaseHelper mDatabaseHelper;
+    private Button signup_btn;
+    private EditText etName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        Button signup_btn = findViewById(R.id.btn_signup);
+        signup_btn = findViewById(R.id.btn_signup);
+        etName = findViewById(R.id.ed_name);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newEntry = etName.getText().toString();
+
+                if(etName.length() != 0) {
+
+                }
+                else {
+                    toastMessage("You must put something in the name field");
+                }
+            }
+        });
 
         signup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +47,22 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void AddData (String newEntry) {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if (insertData) {
+            toastMessage("Data successfully inserted");
+        }
+        else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    private void toastMessage(String message) {
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     @Override
