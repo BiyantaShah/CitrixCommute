2b11a19b05de9faf08bf03ca991eabf89e409e86
poojaba <pooja.babu@citrix.com>
2019-08-12 15:30:02 -0400

    Firebase

--- app/src/main/java/com/example/citrixcommute/Home.java
+++ app/src/main/java/com/example/citrixcommute/Home.java
@@ -1,8 +1,15 @@
 package com.example.citrixcommute;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, Profile.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
